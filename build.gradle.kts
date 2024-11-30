dda5ab129e8b6b5f8a210070c124992e2d4fb29b
Amrita Nair <theamritanair@gmail.com>
2024-12-01 01:21:53 +0530

    chore: add spotless plugin for formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,13 @@ plugins {
     id("io.micronaut.test-resources") version "4.4.4"
     id("io.micronaut.aot") version "4.4.4"
     kotlin("plugin.jpa") version "1.9.25"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 version = "0.1"
 group = "com.event"
 
-val kotlinVersion=project.properties.get("kotlinVersion")
+val kotlinVersion = project.properties.get("kotlinVersion")
 repositories {
     mavenCentral()
 }
@@ -26,30 +27,28 @@ dependencies {
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("io.micronaut.sql:micronaut-jdbc-hikari")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     compileOnly("io.micronaut:micronaut-http-client")
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("io.micronaut:micronaut-http-client")
 
-    //Flyway migrations
+    // Flyway migrations
     implementation("io.micronaut.flyway:micronaut-flyway")
     runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
-    //JPA
+    // JPA
     implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
     implementation("io.micronaut.sql:micronaut-hibernate-jpa")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.yaml:snakeyaml:2.0")
 
-    //OpenAPI
+    // OpenAPI
     ksp("io.micronaut.openapi:micronaut-openapi-annotations")
     annotationProcessor("io.micronaut.openapi:micronaut-openapi")
     implementation("io.swagger.core.v3:swagger-annotations")
-
 }
-
 
 application {
     mainClass = "com.event.Application"
@@ -80,5 +79,17 @@ micronaut {
     }
 }
 
-
-
+spotless {
+    kotlin {
+        ktlint("1.2.1")
+        targetExclude("**/build/generated/**")
+    }
+    kotlinGradle {
+        ktlint("1.2.1")
+    }
+    java {
+        googleJavaFormat()
+        removeUnusedImports()
+        formatAnnotations()
+    }
+}
